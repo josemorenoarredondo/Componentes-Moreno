bd63191771c38ead057fdff2cd10651590f1b985
josemorenoarredondo <jose.moreno@ieeg.org.mx>
2022-11-22 20:33:57 -0600

    Desafio 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './Desafio1/toolbar/toolbar.component';
+import { NavbarComponent } from './Desafio1/navbar/navbar.component';
+import { StudentsComponent } from './Desafio1/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule
